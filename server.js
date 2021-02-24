daa231dfd61b1d49353ee30d01287ed9c40113f4
Aleksandr Romanov <aleksandr.romanov@netcracker.com>
2021-02-24 22:46:43 +0400

    *fxd scripts

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./server/mocks/users.mock.json');
+const middlewares = jsonServer.defaults();
+
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/just-do-test'));
+
+server.use(middlewares);
+server.use(router);
 
 app.get('/*', function(req,res) {
 
@@ -13,3 +21,4 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 3000);
